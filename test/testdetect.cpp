f3db84451397369350022ddeeae2de8760a12be0
taeraemon <dbsl0512@naver.com>
2024-04-01 10:49:24 +0900

    Update logic (text log save)

--- test/testdetect.cpp
+++ test/testdetect.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <dirent.h>
@@ -23,6 +24,7 @@ using namespace zgh;
 
 std::vector<std::string> listFilesInDirectory(const std::string& path);
 std::string createNewFilePath(const std::string& originalPath, const std::string& newDir, const std::string& suffix);
+std::string createNewFilePath(const std::string& originalPath, const std::string& newDir, const std::string& suffix, const std::string& extension);
 
 int main(int argc, char *argv[])
 {
@@ -42,14 +44,22 @@ int main(int argc, char *argv[])
     // Main Detection Loop
     for (const auto& file : fileList) {
         // [1] Directory Initialize
+            // master directory Path
         std::cout << file << std::endl;
         std::string originalFilePath = directoryPath + "/" + file;
         std::string detectPath = directoryPath + "/" + newDir;
+            // Result image Path
+        std::string newFilePath = createNewFilePath(originalFilePath, newDir, "_detect");
+            // Result text Path
+        std::string newTextFilePath = createNewFilePath(originalFilePath, newDir, "_detect", ".txt");
+        std::ofstream outFile(newTextFilePath);
+        if (!outFile.is_open()) {
+            std::cout << "Could not open file for writing: " << newTextFilePath << "\n" << std::endl;
+        }
 
         // [2] Image Open
         cv::Mat board = cv::imread(originalFilePath);
         cv::Mat image = cv::imread(originalFilePath ,cv::IMREAD_GRAYSCALE);
-
         if (image.empty()) {
             std::cout << "Could not open or find the image: " << originalFilePath << std::endl;
             continue;
@@ -62,12 +72,14 @@ int main(int argc, char *argv[])
         double width = 2.0;
         FuncTimerDecorator<int>("detectEllipse")(detectEllipse, image.data, row, col, ells, NONE_POL, width);
         cout << "Find " << ells.size() << " ellipse(s)" << endl;
+        outFile << (int)ells.size() << "\n";
         for (int i = 0; i < (int)ells.size(); ++i) {
             auto ell = ells[i];
-            std::cout << "coverangle : " << ell->coverangle
-                      << ",\tgoodness : " << ell->goodness
-                      << ",\tpolarity : "
-                      << ell->polarity
+            std::cout << "centerX : " << ell->o.x
+                      << "\tcenterY : " << ell->o.y
+                      << "\tshort : " << ell->a
+                      << "\tlong : " << ell->b
+                      << "\tpi/2-phi(deg) : " << rad2angle(PI_2 - ell->phi)
                       << endl;
             cv::ellipse(board,
                         cv::Point(ell->o.y, ell->o.x),
@@ -79,6 +91,9 @@ int main(int argc, char *argv[])
                         width,
                         8,
                         0);
+            outFile << ell->o.x << "\t" << ell->o.y << "\t";
+            outFile << ell->a   << "\t" << ell->b   << "\t";
+            outFile << rad2angle(PI_2 - ell->phi)   << "\n";
         }
 
         // [4] Detection File Save Path inspection
@@ -88,15 +103,16 @@ int main(int argc, char *argv[])
                 continue;
             }
         }
-        
+
         // [5] Result Save
-        std::string newFilePath = createNewFilePath(originalFilePath, newDir, "_detect");
         if (cv::imwrite(newFilePath, board)) {
             std::cout << "Saved: " << newFilePath << std::endl;
         } else {
             std::cout << "Could not save the image: " << newFilePath << std::endl;
         }
-        std::cout << "\n" << std::endl;
+
+        outFile.close();
+        std::cout << "Saved ellipse information to: " << newTextFilePath << "\n" << std::endl;
     }
 
     return 0;
@@ -123,10 +139,10 @@ std::vector<std::string> listFilesInDirectory(const std::string& path) {
 
 std::string createNewFilePath(const std::string& originalPath, const std::string& newDir, const std::string& suffix) {
     std::size_t lastSlashPos = originalPath.rfind('/');
-    if (lastSlashPos == std::string::npos){ // No directory found
-        return originalPath;
+    
+    if (lastSlashPos == std::string::npos){
+        return originalPath; // No directory found
     }
-
     std::string directory = originalPath.substr(0, lastSlashPos);
     std::string fileName = originalPath.substr(lastSlashPos + 1);
     std::size_t dotPos = fileName.rfind('.');
@@ -134,9 +150,26 @@ std::string createNewFilePath(const std::string& originalPath, const std::string
     if (dotPos == std::string::npos){
         return directory + "/" + newDir + "/" + fileName + suffix; // No extension found
     }
-
     std::string nameWithoutExtension = fileName.substr(0, dotPos);
     std::string extension = fileName.substr(dotPos);
+    
+    return directory + "/" + newDir + "/" + nameWithoutExtension + suffix + extension;
+}
+
+std::string createNewFilePath(const std::string& originalPath, const std::string& newDir, const std::string& suffix, const std::string& extension) {
+    std::size_t lastSlashPos = originalPath.rfind('/');
+    
+    if (lastSlashPos == std::string::npos){
+        return originalPath; // No directory found
+    }
+    std::string directory = originalPath.substr(0, lastSlashPos);
+    std::string fileName = originalPath.substr(lastSlashPos + 1);
+    std::size_t dotPos = fileName.rfind('.');
+    
+    if (dotPos == std::string::npos){
+        return directory + "/" + newDir + "/" + fileName + suffix + extension; // No extension found
+    }
+    std::string nameWithoutExtension = fileName.substr(0, dotPos);
     
     return directory + "/" + newDir + "/" + nameWithoutExtension + suffix + extension;
 }
